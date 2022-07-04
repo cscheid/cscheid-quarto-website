23a4cc942cb653f88e1d73c6d02cd251751271e7
Carlos Scheidegger <cscheid@cscheid.net>
2022-07-04 10:36:39 -0700

    update

--- tests/abcjs.ts
+++ tests/abcjs.ts
@@ -1,3 +1,7 @@
+import { initPlayer } from "../src/audio/audio.js";
+import { playNote } from "../src/audio/karplus-strong.js";
+import { notes } from "../src/audio/notes.js";
+
 export function processAbcJs() {
   const nodes = Array.from(document.querySelectorAll("code.abc"));
   for (const node of nodes) {
@@ -6,6 +10,26 @@ export function processAbcJs() {
     const div = pre.parentElement!;
     div.removeChild(pre);
     div.classList.remove("sourceCode");
-    ((window as any).ABCJS as any).renderAbc(div.id, text);
+    const clickListener = (
+      abcelem: any,
+      _tuneNumber: any,
+      _classes: any,
+      _analysis: any,
+      _drag: any,
+      _mouseEvent: any,
+    ) => {
+      for (
+        const pitch of abcelem.pitches.map((pitch: any) =>
+          pitch.pitch
+        ) as number[]
+      ) {
+        initPlayer();
+        playNote(notes[pitch + 24], 1, 0.25);
+      }
+    };
+    const abcobj = ((window as any).ABCJS as any).renderAbc(div.id, text, {
+      clickListener,
+    });
+    debugger;
   }
 }
